4aafd754293082e34fe1c0b5c59f380de8737848
raja9112 <rrn.rajarajan@gmail.com>
2024-08-30 04:24:33 +0530

    Made some changes on templates, Updated chatapp/urls.py and routed to correct views functions and rendered all templates for get method and created post method for registration page and manually tested registration process. Works fine!

--- chatproject/chatapp/urls.py
+++ chatproject/chatapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .import views
+from .views import Home, ChangePassword, Logout, Login, Register, Recover_password
 
 urlpatterns = [
-    path('',views.home, name = "HomePage"),
+    path('', Home.as_view(), name = "HomePage"),
+    path('auth-changepassword/', ChangePassword.as_view(), name= 'Change_password'),
+    path('auth-logout/', Logout.as_view(), name= "Logout page"),
+    path('auth-login/', Login.as_view(), name="Login page"),
+    path('auth-register/', Register.as_view(), name="Registration page"),
+    path('auth-recoverpw/', Recover_password.as_view(), name="Forget password page")
+
 
     
 ]
--- chatproject/chatapp/views.py
+++ chatproject/chatapp/views.py
@@ -1,5 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
 
 # Create your views here.
-def home(request):
-    return render(request, 'index.html')
+
+class Home(View):
+    def get(self, request):
+        return render(request, 'index.html')
+    def post(self, request):
+        pass
+
+class ChangePassword(View):
+    def get(self, request):
+        return render(request, 'auth-changepassword.html')
+    def post(self, request):
+        pass
+
+class Logout(View):
+    def get(self, request):
+        return render(request, 'auth-logout.html')
+    def post(self, request):
+        pass
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'auth-login.html')
+    def post(self, request):
+        pass
+
+class Register(View):
+    def get(self, request):
+        return render(request, 'auth-register.html')
+    
+    def post(self, request):
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirmpassword = request.POST.get('confirmpassword')
+
+        if password == confirmpassword:
+            if User.objects.filter(username = username).exists():
+                messages.error(request, "Username already taken. Please try again")
+            creating_user = User.objects.create_user(username = username, email= email , password = confirmpassword)
+            creating_user.save()
+
+            auth.login(request, creating_user)
+            messages.success(request, "Welcome to Kooku, An App by Tea Kadai!")
+            return redirect('/')
+        
+        messages.info(request, "Password did not match")
+        return redirect('auth-register/')
+
+
+class Recover_password(View):
+    def get(self, request):
+        return render(request, 'auth-recoverpw.html')
+    def post(self, request):
+        pass
